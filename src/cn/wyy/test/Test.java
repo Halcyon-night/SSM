933dfba3c0ce0630ff1857777534610af0b3c537
Halcyon-night <68455105+Halcyon-night@users.noreply.github.com>
2020-07-22 17:02:33 +0800

    Update Test.java

--- src/cn/wyy/test/Test.java
+++ src/cn/wyy/test/Test.java
@@ -3,5 +3,7 @@ package cn.wyy.test;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("123456789");
+		System.out.println("修改远程配置信息");
+		System.out.println("123456789");
 	}
 }
